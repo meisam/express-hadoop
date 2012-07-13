46f3af5704ca5b5943a8c66552f75d157436e355
Siyuan Ma <siyuan.ma9@gmail.com>
2012-07-13 14:30:26 -0500

    cc-xrec-test

--- src/express/hdd/ConflictCalculator.java
+++ src/express/hdd/ConflictCalculator.java
@@ -5,8 +5,10 @@ import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileStatus;
@@ -97,7 +99,7 @@ public class ConflictCalculator extends Configured implements Tool {
 			ArrayList<Pair<int[], int[]>> recs = new ArrayList<Pair<int[], int[]>>(); 
 			while (keys.hasNext()){
 				Text key = keys.next();
-				writer.append(key, new Text("1")); //intersection(recs), #rec 
+				//writer.append(key, new Text("1")); //intersection(recs), #rec 
 				try {
 					recs.add(Tools.text2Pair(key));
 				} catch (Exception e) {
@@ -112,16 +114,66 @@ public class ConflictCalculator extends Configured implements Tool {
 					Pair<int[], int[]> conflict = HyperRectangleData.getHyperRectangleIntersection(recs.get(i), recs.get(j));
 					if (conflict != null)
 						try {
-							writer.append(Tools.pair2Text(conflict), new Text("2"));
+							//writer.append(Tools.pair2Text(conflict), new Text("2"));
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} 
 				}
 			
+			ArrayList<ArrayList<Pair<int[], int[]>>> conflictList = new ArrayList<ArrayList<Pair<int[], int[]>>> ();
+			ArrayList<ArrayList<Set<Integer>>> conflictID = new ArrayList<ArrayList<Set<Integer>>> ();
+			calculateConflicts(recs, conflictList, conflictID);
+			try {
+				dumpConflictList(conflictList, writer);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			writer.close();
 		}
 	}
+	
+	private static void calculateConflicts(ArrayList<Pair<int[], int[]>> recs, 
+			ArrayList<ArrayList<Pair<int[], int[]>>> conflictList, ArrayList<ArrayList<Set<Integer>>> conflictID) {
+		ArrayList<Set<Integer>> recIDList1 = new ArrayList<Set<Integer>>();
+		for (int i=0; i<recs.size(); i++){
+			recIDList1.add(new HashSet<Integer>(Arrays.asList(i)) );
+		}
+		
+		conflictList.add((ArrayList<Pair<int[], int[]>>) recs.clone()); //0: all rectangles
+		conflictID.add(recIDList1);
+		
+		ArrayList<Pair<int[], int[]>> Conflict2List = new ArrayList<Pair<int[], int[]>>();
+		ArrayList<Set<Integer>> recIDList2 = new ArrayList<Set<Integer>>();
+		for (int i=0; i<recs.size(); i++)
+			for (int j=i+1; j<recs.size(); j++){
+				Pair<int[], int[]> conflict = HyperRectangleData.getHyperRectangleIntersection(recs.get(i), recs.get(j));
+				if (conflict != null)
+					try {
+						Conflict2List.add(conflict);
+						recIDList2.add(new HashSet<Integer>(Arrays.asList(i,j)) );
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+			}
+			
+	}
+	
+	private static void dumpConflictList(ArrayList<ArrayList<Pair<int[], int[]>>> conflictList, 
+			SequenceFile.Writer writer) throws IOException, Exception {
+		if (conflictList == null)
+			return;
+		for (int i=0; i<conflictList.size(); i++) {
+			ArrayList<Pair<int[], int[]>> list = conflictList.get(i);
+			for (int j=0; j<list.size(); j++){
+				writer.append(Tools.pair2Text(list.get(j)), new Text(Integer.toString(i+1)));
+			}
+		}
+	}
+		
 	
 	@Override
 	public int run(String[] args) throws Exception {
