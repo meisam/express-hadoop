61009e8892f3f7ff06f7e83258c7c5a6f653f85b
Siyuan Ma <siyuan.ma9@gmail.com>
2012-07-20 11:05:33 -0500

    add outputValueGroupingComparator

--- src/express/hdd/HDFIntermediateKey.java
+++ src/express/hdd/HDFIntermediateKey.java
@@ -3,15 +3,17 @@ package express.hdd;
 import org.apache.hadoop.io.Text;
 
 public class HDFIntermediateKey {
+	static final String splitToken = "@_@";
+	
 	public static Text merge(Text naturalKey, Text secondaryKey) {
-		return new Text(naturalKey.toString() + "+" + secondaryKey.toString());
+		return new Text(naturalKey.toString() + splitToken + secondaryKey.toString());
 	}
 	
 	public static Text getNaturalKey(Text IntermediateKey){
-		return new Text(IntermediateKey.toString().split("+")[0]);
+		return new Text(IntermediateKey.toString().split(splitToken)[0]);
 	}
 	
 	public static Text getSecondaryKey(Text IntermediateKey){
-		return new Text(IntermediateKey.toString().split("+")[1]);
+		return new Text(IntermediateKey.toString().split(splitToken)[1]);
 	}
 }
