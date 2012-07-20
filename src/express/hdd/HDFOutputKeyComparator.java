9a2e46ddac15d63df54bef9fe8f56c8579a37304
Siyuan Ma <siyuan.ma9@gmail.com>
2012-07-20 16:39:29 -0500

    add secondarySorting

--- src/express/hdd/HDFOutputKeyComparator.java
+++ src/express/hdd/HDFOutputKeyComparator.java
@@ -9,20 +9,20 @@ public class HDFOutputKeyComparator implements RawComparator<Text> {
 
 	@Override
 	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-		//return compare(new Text(Arrays.copyOfRange(b1, s1, l1)), new Text(Arrays.copyOfRange(b2, s2, l2)));
-		return compare(new Text(b1), new Text(b2));
+		return compare(new Text(Arrays.copyOfRange(b1, s1, l1)), new Text(Arrays.copyOfRange(b2, s2, l2)));
+		//return compare(new Text(b1), new Text(b2));
 	}
 
 	@Override
 	public int compare(Text tx, Text ty) {
 		try {
 //			System.out.printf("HDFOutputKeyComparator: x=%s and y=%s\n", tx.toString(), ty.toString());
-//			Pair<int[], int[]> x = Tools.text2Pair(HDFIntermediateKey.getSecondaryKey(tx));
-//			Pair<int[], int[]> y = Tools.text2Pair(HDFIntermediateKey.getSecondaryKey(ty));
-//				
-//			if (HyperRectangleData.hasIncreasingChunkNumber(x, y))
-//				return -1;
-//			else
+			Pair<int[], int[]> x = Tools.text2Pair(HDFIntermediateKey.getSecondaryKey(tx));
+			Pair<int[], int[]> y = Tools.text2Pair(HDFIntermediateKey.getSecondaryKey(ty));
+				
+			if (HyperRectangleData.hasIncreasingChunkNumber(x, y))
+				return -1;
+			else
 				return 1;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
