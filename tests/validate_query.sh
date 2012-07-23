7e48223972ee46c35f5fed31ffd58f451163ae83
Siyuan Ma <siyuan.ma9@gmail.com>
2012-07-22 19:04:20 -0500

    add HDFSetQuery

--- tests/validate_query.sh
+++ tests/validate_query.sh
@@ -17,4 +17,17 @@ oneQuery(){
 	$TESTDIR/queryBase.sh pureQuery ${RECINFO} "32,32,32" "128,128,128" ${INDIR} ${OUTDIR};
 }
 
+twoQueries(){
+	echo "oneQuery $@";
+	
+	RECINFO="1024,512,512 0,0,0 64,512,512 1,1,1";
+	INDIR=hdf-test;
+	OUTDIR=hdf-query;
+	
+	$EXEC fs -rmr ${INDIR};
+	$EXEC fs -rmr ${OUTDIR};
+	$TESTDIR/genBase.sh pureGen ${RECINFO} ${INDIR};
+	$TESTDIR/queryBase.sh pureQuery ${RECINFO} "32,32,32 256,256,256" "128,128,128 192,128,64" ${INDIR} ${OUTDIR};
+}
+
 eval "$@";
